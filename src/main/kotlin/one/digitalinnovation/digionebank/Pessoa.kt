245db7c76e783b923afc30127741a4b32662828d
Joseph-Yannick <www.josyan9_jyzk@hotmail.com>
2021-03-18 05:58:33 -0300

    Update to talk about class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,19 +1,20 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
-    var name: String = "Joseph"
-
+    var nome: String = "Joseph"
     var cpf: String = "123.456.789-00"
     private set
 
+    constructor()
 
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val joseph = Pessoa ()
+    println (joseph.pessoaInfo())
 
-    println (joseph.name)
-    println (joseph.cpf)
 
 
 
