32544f85268fed7b0aa2983eb8234afc6a1212e8
Joseph-Yannick <www.josyan9_jyzk@hotmail.com>
2021-03-18 06:24:44 -0300

    Created account class

--- src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
@@ -1,11 +1,11 @@
 package one.digitalinnovation.digionebank
 
 import java.math.BigDecimal
-import java.math.BigInteger
+
 
 class Conta(
-    val agencia: String
-    val numero: String
+    val agencia: String,
+    val numero: String,
     val saldo: BigDecimal
 
 ) {
