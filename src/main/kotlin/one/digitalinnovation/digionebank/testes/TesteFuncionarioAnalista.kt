144a710a8d34636101b019a2f11f8544d8231a60
Joseph-Yannick <www.josyan9_jyzk@hotmail.com>
2021-03-18 12:24:18 -0300

    Created Tests to Manager, Analist and more

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioAnalista.kt
@@ -0,0 +1,14 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Analista
+
+
+
+fun main() {
+
+    val ana = Analista ( nome = "Ana Luiza", cpf = "033.456.789-27",  2035.0)
+    ImprimeRelatorioFuncionario.imprime(ana)
+
+}
+
+
---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -0,0 +1,18 @@
+package one.digitalinnovation.digionebank.testes
+
+
+
+
+import one.digitalinnovation.digionebank.Gerente
+
+
+
+fun main() {
+
+    val vilma = Gerente ( nome = "Vilma Cerqueira", cpf = "123.456.789-20",  5520.0)
+    ImprimeRelatorioFuncionario.imprime(vilma)
+
+
+}
+
+
